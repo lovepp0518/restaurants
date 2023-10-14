41bd249cf70ca542d0650c1236a314cabc78c735
lovepp0518 <lovepp0518@gmail.com>
2023-10-15 03:00:32 +0800

    build: use express-handlebars as view engine

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const { engine } = require('express-handlebars') //使用express-handlebars
 const app = express()
 const port = 3000
+
+// 把樣板引擎交給express-handlebars
+app.engine('.hbs', engine({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
+app.set('views', './views')
 
 // 呼叫靜態檔案json
 app.use(express.static('public'))
@@ -12,7 +18,7 @@ app.get('/', (req, res) => {
 
 // restaurants載入清單
 app.get('/restaurants', (req, res) => {
-  res.send('restaurant list')
+  res.render('index')
 })
 
 app.get('/restaurant/:id', (req, res) => {
