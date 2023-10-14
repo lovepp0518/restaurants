0f03a5eded64a5aece947067aa0872a3a7f374a6
lovepp0518 <lovepp0518@gmail.com>
2023-10-15 02:18:37 +0800

    build: route & controller setup

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// 首頁重新導入restaurants
 app.get('/', (req, res) => {
-  res.send('express app for restaurants')
+  res.redirect('/restaurants')
+})
+
+// restaurants載入清單
+app.get('/restaurants', (req, res) => {
+  res.send('restaurant list')
+})
+
+app.get('/restaurant/:id', (req, res) => {
+  const id = req.params.id
+  res.send(`restaurant: ${id}`)
 })
 
 app.listen(port, () => {
