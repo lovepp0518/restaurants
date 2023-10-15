9fdb778a4e53a7e847e3a5b0831e3221cde9e191
lovepp0518 <lovepp0518@gmail.com>
2023-10-15 16:18:00 +0800

    feat:add show page

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ app.get('/restaurants', (req, res) => {
 
 app.get('/restaurant/:id', (req, res) => {
   const id = req.params.id
-  res.send(`restaurant: ${id}`)
+  const restaurant = restaurants.find((restaurant) => restaurant.id.toString() === id)
+  res.render('show', { restaurant })
 })
 
 app.listen(port, () => {
